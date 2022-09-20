12a4f1a0e29ff9c4a7efb80651cf14ce40746318
JohnZhou0821 <113874059+JohnZhou0821@users.noreply.github.com>
2022-09-20 17:51:30 -0400

    Update alive.java

--- into to 0007/src/alive.java
+++ into to 0007/src/alive.java
@@ -3,14 +3,14 @@ public class alive {
         String fName="John (Xilin) ";
         String lName="Zhou";
         System.out.println(fName+ lName);
-        int birthday=2003;
-        int asOfDate=2022;
+        double birthday=2003; //08/21/2003 10am
+        double asOfDate=2022; //09/16/2003 10am
         System.out.println("I am " +(asOfDate-birthday) +" years old");
-        System.out.println("I am " +((asOfDate-birthday)*12) +" months old");
-        System.out.println("I am " +((asOfDate-birthday)*12*365*24) +" hours old");
-        System.out.println("I am " +((asOfDate-birthday)*12*365*24*60) +" minutes old");
+        System.out.println("I am " +((asOfDate-birthday)*12+1) +" months old");
+        System.out.println("I am " +(((asOfDate-birthday)*12+1)*30.4167*24+(26*24)) +" hours old");
+        System.out.println("I am " +(((asOfDate-birthday)*12+1)*30.4167*24*60+(26*24*60)) +" minutes old");
         float weekDaySleepAmountPerDay=6;
         float weekEndSleepAmountPerDay=7;
         System.out.println("The average amount of sleep I get is " +((weekEndSleepAmountPerDay+weekDaySleepAmountPerDay)/2) +" hours");
+        }
     }
-}
